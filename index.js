6b487387674a4f14658e75e5e5568f974a9cbebd
Rodrigo Cunha <rodrigo.cunha@wiline.com>
2018-03-29 06:59:29 -0700

    added the js to the index.js file for the modal

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 import resources from '/js/resources';
 import app from '/js/app';
 import engine from '/js/engine';
+
+// Get the modal
+var modal = document.getElementById('startGame');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+window.onload = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
